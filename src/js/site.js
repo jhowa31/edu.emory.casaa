4c7a3ceafbd5053dde81d73ca636f4401ce82232
jhowa31 <32077853+jhowa31@users.noreply.github.com>
2017-11-16 18:17:55 -0500

    homepage opener

--- src/js/site.js
+++ src/js/site.js
@@ -1,1 +1,10 @@
 // Plain Javascript 
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 90) {
+        $(".navbar-campus-life").addClass("scrolling");
+    } else {
+        $(".navbar-campus-life").removeClass("scrolling");
+    }
+});
