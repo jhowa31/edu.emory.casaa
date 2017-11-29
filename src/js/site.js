ca67942463f6441f125b691722a198b661de8e38
Kayla Pratt <lockepratt@gmail.com>
2017-11-29 09:53:31 -0500

    Add files via upload
    Swiper Image Gallery added
    Person Row with/without image example
    Sweeping WCAG 2.0 AA changes to color contrast
    Owl Carousel and Swiper Carousel initializations and settings moved to js/site.js

--- src/js/site.js
+++ src/js/site.js
@@ -8,3 +8,57 @@ $(window).scroll(function() {
         $(".navbar-campus-life").removeClass("scrolling");
     }
 });
+
+/*
+ * Initializing and Settings
+ * 
+ */
+
+// Testimonial Carousel
+$('.testimonial-carousel').owlCarousel({
+    items:2,
+    loop:true,
+    dots:true,
+    margin:15,
+    autoplay:true,
+	autoplayHoverPause:true,
+	responsiveClass:true,
+	responsive:{
+		0:{
+			items:1
+		},
+		768:{
+			items:1
+		},
+		992:{
+			items:2
+		}
+	}
+});
+
+// Two-column Carousel
+$('.twoColumn-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    dots:true,
+    autoplay:true,
+    autoplayHoverPause:true
+});
+
+// Swiper Gallery
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
